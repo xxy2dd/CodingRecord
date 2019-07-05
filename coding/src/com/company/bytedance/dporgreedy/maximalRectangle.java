6b632c5cac01648140b7930ee9dce7ba9ae65786
xxy2dd <xxy_cpu@163.com>
2019-07-05 15:14:52 +0800

    code of sorts

--- coding/src/com/company/bytedance/dporgreedy/maximalRectangle.java
+++ coding/src/com/company/bytedance/dporgreedy/maximalRectangle.java
@@ -7,36 +7,57 @@ package com.company.bytedance.dporgreedy;
  * 给定一个仅包含 0 和 1 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积
  */
 public class maximalRectangle {
-    public int maximalSquare(char[][] matrix) {
-        if(matrix.length==0||matrix[0].length==0){
+    public static void main(String[] args){
+        maximalRectangle m = new maximalRectangle();
+        char[][] c = {{'1','0','1','0'},{'0', '1','0','1'},{'1','1', '1','1'},{'1','1','1','1'},{'0','0','1','0'}};
+
+        m.maximalRectangle(c);
+    }
+    public int maximalRectangle(char[][] matrix) {
+        if(matrix.length==0){
             return 0;
         }
-        int row = matrix.length;
-        int col = matrix[0].length;
-        int res = 0;
-        int[][] dp = new int[row][col];
-        for(int i=0;i<row;i++){
-            if(matrix[i][0]=='1'){
-                dp[i][0] = 1;
-                res = 1;
-            }
+        int result = 0;
+        int row = matrix.length,col = matrix[0].length;
+        // 记录左边界
+        int[] L = new int[col];
+        // 记录右边界
+        int[] R = new int[col];
+        // 记录高
+        int[] H = new int[col];
+        // 初始化
+        for(int i=0;i<col;i++){
+            L[i] = 0;
+            R[i] = col;
+            H[i] = 0;
         }
-        for(int j=0;j<col;j++){
-            if(matrix[0][j]=='1'){
-                dp[0][j] = 1;
-                res = 1;
-
-            }
-        }
-        for(int i=1;i<row;i++){
-            for(int j=1;j<col;j++){
+        // 遍历每一行
+        for(int i = 0;i<row;i++){
+            int left = 0;
+            for(int j = 0;j<col;j++){
+                // 遇到 1 ，左边界取L[j] 和 left较大者，高度+1
                 if(matrix[i][j]=='1'){
-                    dp[i][j] = Math.min(Math.min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1])+1;
+                    L[j] = Math.max(L[j],left);
+                    H[j] +=1;
+                }else{
+                    // 遇到0，所有状态归位, left = j+1
+                    L[j] = 0;
+                    R[j] = col;
+                    H[j] = 0;
+                    left=j+ 1;
                 }
-                res = Math.max(res,dp[i][j]);
+            }
+            // 从右向左考虑右边界
+            int right = col;
+            for(int j=col-1;j>=0;j--){
+                if(matrix[i][j]=='1'){
+                    R[j] = Math.min(R[j],right);
+                    result = Math.max(result,H[j]*(R[j]-L[j]));
+                }else{
+                    right = j;
+                }
             }
         }
-        return res*res;
-
+        return result;
     }
 }
--- coding/src/com/company/nowcoder/Match.java
+++ coding/src/com/company/nowcoder/Match.java
@@ -6,5 +6,41 @@ package com.company.nowcoder;
  * @description
  */
 public class Match {
+    public static void main(String[] args){
+        String str = "aaa";
+        String pattern = "ab*ac*a";
+        Match m = new Match();
+        m.match(str.toCharArray(),pattern.toCharArray());
+    }
+    public boolean match(char[] str, char[] pattern)
+    {
+        if(str==null && pattern==null){
+            return true;
+        }
+        if(str!=null && pattern==null){
+            return false;
+        }
+        int sindex = 0, pindex = 0;
+        return matchCore(str, sindex, pindex, pattern);
+    }
+    public boolean matchCore(char[] str, int sindex, int pindex, char[] pattern){
+        if(sindex >= str.length && pindex == pattern.length)
+            return true;
+        if(pindex >= pattern.length && sindex < str.length)
+            return false;
+        if(pindex+1 < pattern.length && pattern[pindex+1] == '*'){
+            if(sindex < str.length && (str[sindex] == pattern[pindex] || pattern[pindex] == '.') ){
+                return matchCore(str, sindex, pindex+2, pattern) ||
+                        matchCore(str, sindex+1, pindex+2, pattern ) ||
+                        matchCore(str, sindex+1, pindex, pattern);
+            }else{
+                return matchCore(str, sindex, pindex+2, pattern);
+            }
+        }
+        if(sindex < str.length && (str[sindex] == pattern[pindex] || pattern[pindex] == '.'))
+            return matchCore(str, sindex+1, pindex+1, pattern);
+        return false;
+
+    }
 
 }

6c98be72e42e616c1fe11999cec7d0499082cf31
xxy2dd <xxy_cpu@163.com>
2019-09-17 14:32:35 +0800

    arrays

---
+++ coding/src/com/company/leetcode/arrays/merge.java
@@ -0,0 +1,18 @@
+package com.company.leetcode.arrays;
+
+/**
+ * @author xxy
+ * @date 2019/9/17
+ * @description
+ */
+public class merge {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int len1 = m-1;
+        int len2 = n-1;
+        int len = m+n-1;
+        while(len1>=0&&len2>=0){
+           nums1[len--] = nums1[len1]>nums2[len2]?nums1[len1--]:nums2[len2--];
+        }
+        System.arraycopy(nums2,0,nums1,0,len2+1);
+    }
+}

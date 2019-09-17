b185b3c616e5455d23460abfce4f344f1456d39e
xxy2dd <xxy_cpu@163.com>
2019-09-17 14:32:04 +0800

    dfs

--- coding/src/com/company/leetcode/dfs/generateParenthesis.java
+++ coding/src/com/company/leetcode/dfs/generateParenthesis.java
@@ -1,5 +1,8 @@
 package com.company.leetcode.dfs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author xxy
  * @date 2019/8/5
@@ -10,4 +13,23 @@ package com.company.leetcode.dfs;
  *
  */
 public class generateParenthesis {
+    List<String> res = new ArrayList<>();
+    public List<String> generateParenthesis(int n) {
+
+        add("",0,0,n);
+        return res;
+    }
+    public void add(String s,int left,int right,int n){
+        if(s.length()==2*n){
+            res.add(s);
+            return ;
+        }
+        if(left<n){
+            add(s+"(",left+1,right,n);
+        }
+        if(left>right){
+
+            add(s+")",left,right+1,n);
+        }
+    }
 }
